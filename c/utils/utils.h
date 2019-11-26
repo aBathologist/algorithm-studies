7a3862f144d5f15c092800857ffbe51bab494a97
Shon Feder <shon.feder@gmail.com>
2019-11-25 22:23:54 -0500

    Remove unused struct

--- c/utils/utils.h
+++ c/utils/utils.h
@@ -7,11 +7,6 @@ void print_array(int arry[], int len);
 void print_matrix(int n, int m[n][n]);
 bool is_substring(char sub[], char str[]);
 
-struct test_s {
-  char *name;
-  bool result;
-};
-
 void test(char test_name[], bool test_result, bool results);
 
 #endif // __UTILS_H_
